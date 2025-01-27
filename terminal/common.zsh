97a29d98517826abae48ed149dc606d7911f58c0
Justin Harris <juharris@users.noreply.github.com>
2025-01-26 20:42:00 -0500

    zsh: Simplify setting colors + add bold + underline

--- terminal/common.zsh
+++ terminal/common.zsh
@@ -1,16 +1,19 @@
 # For a nice PS1.
-# Cannot be done in bash.
-autoload -U colors && colors
+
+# `autoload` cannot be used in bash.
+# It was needed when using `%{$fg[cyan]%}`, but seems like it's not needed with `%F{cyan}`.
+# autoload -U colors && colors
 
 function parse_git_branch {
 branch=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
 if [ -n "${branch}" ]; then
-    echo "(%{$fg[cyan]%}$branch%{$reset_color%})"
+    echo "(%F{cyan}%U$branch%u%f)"
 fi
 }
 
 setopt PROMPT_SUBST;
-PS1="%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[green]%}%m %{$fg[yellow]%}%~ %{$reset_color%}\$(parse_git_branch) "$'\n'"%% "
+# `%#`: `#` for root, `%` for user
+PS1="%F{magenta}%n%f@%F{green}%m%f %F{yellow}%B%~%b%f \$(parse_git_branch) "$'\n'"%# "
 
 _current_dir=`dirname "$(realpath $0)"`
 BASH_SOURCE="${_current_dir}/common.sh" emulate ksh -c '. "$BASH_SOURCE"'
