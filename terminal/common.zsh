f6b1859aacff4b6d1b57dc06bd6f8fc89dfe0c00
Justin D. Harris <juharris@users.noreply.github.com>
2025-07-12 19:42:11 -0400

    [Bash] Show branch in cyan

--- terminal/common.zsh
+++ terminal/common.zsh
@@ -4,10 +4,10 @@
 # It was needed when using `%{$fg[cyan]%}`, but seems like it's not needed with `%F{cyan}`.
 # autoload -U colors && colors
 
-function parse_git_branch {
+function show_git_branch {
 branch=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
 if [ -n "${branch}" ]; then
-    echo "(%F{cyan}%U$branch%u%f)"
+    echo " (%F{cyan}%U$branch%u%f)"
 fi
 }
 
@@ -15,7 +15,7 @@ setopt PROMPT_SUBST;
 # `%#` at the end of the line with show `#` for root, `%` for user.
 # Used to use `%m` for the ${HOST}, but it would change the HOST which is annoying when other programs change it.
 _host="$(hostname -s)"
-PS1="%(?..%F{red}[%?]%f )%F{magenta}%n%f@%F{green}${_host}%f %F{yellow}%B%~%b%f \$(parse_git_branch) "$'\n'"%# "
+PS1="%(?..%F{red}[%?]%f )%F{magenta}%n%f@%F{green}${_host}%f %F{yellow}%B%~%b%f\$(show_git_branch) "$'\n'"%# "
 
 # Case insensitive completion
 autoload -Uz compinit && compinit
