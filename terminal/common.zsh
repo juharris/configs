46bb729c839a55662ff6743bbb833bd2e2062e33
Justin Harris <juharris@users.noreply.github.com>
2026-06-12 16:08:25 -0400

    common.zsh: Add + when there are staged files

--- terminal/common.zsh
+++ terminal/common.zsh
@@ -5,14 +5,42 @@
 # autoload -U colors && colors
 
 function show_git_branch {
-branch=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
+local branch change_modifiers git_status has_staged_changes has_unstaged_changes
+branch="$(command git symbolic-ref --quiet --short HEAD 2> /dev/null)"
+if [ -z "${branch}" ]; then
+    branch="$(command git rev-parse --short HEAD 2> /dev/null)"
+fi
+
 if [ -n "${branch}" ]; then
-    changes=`git status --porcelain 2> /dev/null`
-    change_modifier=""
-    if [ -n "${changes}" ]; then
-        change_modifier="*"
+    command git diff-index --quiet --cached HEAD -- 2> /dev/null
+    git_status=$?
+    case "${git_status}" in
+        1)
+            has_staged_changes=1
+            ;;
+        128)
+            if ! command git diff --no-ext-diff --cached --quiet -- 2> /dev/null; then
+                has_staged_changes=1
+            fi
+            ;;
+    esac
+
+    if ! command git diff --no-ext-diff --quiet -- 2> /dev/null; then
+        has_unstaged_changes=1
+    elif command git ls-files --others --exclude-standard --directory --no-empty-directory -- ':/' 2> /dev/null | read -r; then
+        has_unstaged_changes=1
     fi
-    echo " (%F{cyan}%U$branch%u%f${change_modifier})"
+
+    change_modifiers=""
+    if [ -n "${has_staged_changes}" ]; then
+        change_modifiers="+"
+    fi
+
+    if [ -n "${has_unstaged_changes}" ]; then
+        change_modifiers="${change_modifiers}*"
+    fi
+
+    echo " (%F{cyan}%U$branch%u%f${change_modifiers})"
 fi
 }
 
